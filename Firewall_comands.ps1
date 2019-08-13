e47b537a0ad3c1c9707294849303476147ff8346
Gabriel Rios <griosc@canvia.com>
2019-08-13 10:13:34 -0500

    Sprint 3

--- Firewall_comands.ps1
+++ Firewall_comands.ps1
@@ -1,1 +1,8 @@
 #!/usr/bin/pwsh 
+param( 
+  [string]$vcenter, 
+  [string]$username, 
+  [string]$password 
+)
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false -Scope User -ParticipateInCEIP:$false | Out-Null 
+Connect-VIServer -Server $vcenter -Protocol https -User $username -Password $password | Out-Null
