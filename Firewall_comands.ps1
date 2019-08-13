80f4bbb0db867fcd27a420fda1d63971708d3947
Gabriel Rios <griosc@canvia.com>
2019-08-13 12:09:14 -0500

    Sprint 3

--- Firewall_comands.ps1
+++ Firewall_comands.ps1
@@ -5,5 +5,5 @@ param(
   [string]$password 
 )
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false -Scope User -ParticipateInCEIP:$false | Out-Null 
-New-SSHSession -ComputerName $ip_firewall -Credential $username | Out-Null
+New-SSHSession -ComputerName $ip_firewall -Credential $username $password | Out-Null
 Invoke-SSHCommand -Index 0 -Command "show security flow session summary node primary"
